490e396f993fec2c371bd1efb89a6680c4187283
engamohd <30385088+engamohd@users.noreply.github.com>
2017-09-06 09:03:15 +0300

    v2.3 Tweaked search form style

--- searchform.php
+++ searchform.php
@@ -3,9 +3,8 @@
 
     <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 
-      <input type="search" class="search-field h-100 p-2" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+      <input type="search" class="search-field rounded h-100 p-3 m-3" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 
-      <input type="submit" class="btn btn-outline-dark btn-xs search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
     </form>
 
   </div>
